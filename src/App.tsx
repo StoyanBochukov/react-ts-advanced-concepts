64c19caf21a78d690bf7ec4b0d718862aca6c771
Stoyan Bochukov <73019209+StoyanBochukov@users.noreply.github.com>
2024-01-06 20:23:42 +0000

    context api with tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,21 @@
-import Input from "./components/Input";
-import Button from "./components/Button";
-import Container from "./components/Container";
-import Card from "./components/Card";
-import Form, { type FormHandleType } from "./components/Form";
-import { useRef } from "react";
+
+import Header from "./components/Header";
+import AddTimer from "./components/AddTimers";
+import Timers from "./components/Timers";
+import TimersContextProvider from "./store/timers-context";
 
 
 function App() {
 
-  const input = useRef<HTMLInputElement>(null)
-  const customFormRef = useRef<FormHandleType>(null)
-  const handleSave = (data: unknown) => {
-    const extractedData = data as { name: string; age: string };
-    console.log(extractedData);
-    customFormRef.current?.clear()
-    
-  }
-
-  return <main>
-    <Input id="name" label="Your Name" type="text" ref={input} />
-    <Input id="age" label="Your Age" type="number" />
-    <Button el="button">A Button</Button>
-    <Button el="link" href="https://google.com" target="_blank">A Link</Button>
-    <Container as={Button} el="button">Click Me</Container>
-    <Card title="Demo Card" actions={
-      <button onClick={() => {console.log('Button is Clicked');
-      }}>Demo Button.. Click Me</button>
-    }>
-      <div>
-        <ul>
-          <li>Some Text</li>
-          <li>Some Text</li>
-        </ul>
-        <p>Some More Demo Content</p>
-      </div>
-    </Card>
-    <Form onSave={handleSave} ref={customFormRef}>
-      <Input type="text" label="Some Demo Text" id="demo-id" />
-      <Input type="text" label="More Demo Text" id="another-demo-id" />
-      <p>
-        <Button el="button">Submit</Button>
-      </p>
-    </Form>
-  </main>;
+  return (
+    <TimersContextProvider>
+      <Header />
+      <main>
+        <AddTimer />
+        <Timers />
+      </main>
+    </TimersContextProvider>
+  )
 }
 
 export default App;
